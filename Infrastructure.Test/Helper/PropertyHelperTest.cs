6fab06d66ee9d4a5081924e02f0e18488684217f
yangzw <yangzw@Ctrip.com>
2015-05-12 15:27:18 +0800

    Add PropertyHelper

---
+++ Infrastructure.Test/Helper/PropertyHelperTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.Helper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Infrastructure.Test.Helper
+{
+    [TestClass]
+    public class PropertyHelperTest
+    {
+        private class Person
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+        }
+
+        [TestMethod]
+        public void TestGetSet()
+        {
+            var person = new Person {Id = 1, Name = "张三"};
+            Assert.AreEqual(person.Id, PropertyHelper<Person>.GetProperty(person, "Id"));
+            const int newId = 2;
+            PropertyHelper<Person>.SetProperty(person, "Id", newId);
+            Assert.AreEqual(newId, PropertyHelper<Person>.GetProperty(person, "Id"));
+        }
+
+        [TestMethod]
+        public void TestFillModel()
+        {
+            var dataTable = new DataTable();
+            dataTable.Columns.Add("Id", typeof (int));
+            dataTable.Columns.Add("Name", typeof(string));
+            var person = new Person { Id = 1, Name = "张三" };
+            var dataRow = dataTable.NewRow();
+            PropertyHelper<Person>.FillDataRow(person, ref dataRow);
+            Assert.AreEqual(person.Id.ToString(), dataRow["Id"].ToString());
+            Assert.AreEqual(person.Name.ToString(), dataRow["Name"].ToString());
+        }
+
+        [TestMethod]
+        public void TestFillEntity()
+        {
+            var dataTable = new DataTable();
+            dataTable.Columns.Add("Id", typeof(int));
+            dataTable.Columns.Add("Name", typeof(string));
+            var dataRow = dataTable.NewRow();
+            dataRow["Id"] = 1;
+            dataRow["Name"] = "李四";
+
+            var entity = PropertyHelper<Person>.FillEntity(dataRow);
+            Assert.AreEqual(dataRow["Id"], entity.Id);
+            Assert.AreEqual(dataRow["Name"], entity.Name);
+        }
+    }
+}
---
+++ Infrastructure/Helper/PropertyHelper.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Helper
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class Column : Attribute
+    {
+        public string Name { get; set; }
+
+        public Column(string name)
+        {
+            if (String.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException();
+            }
+
+            this.Name = name;
+        }
+    }
+
+    public partial class PropertyHelper<T> where T : class, new()
+    {
+        private PropertyHelper()
+        {
+        }
+
+        private static readonly Dictionary<string, Func<T, object>> GetDelegateDict = new Dictionary<string, Func<T, object>>();
+        private static readonly Dictionary<string, Action<T, object>> SetDelegateDict = new Dictionary<string, Action<T, object>>();
+
+        static PropertyHelper()
+        {
+            var type = typeof(T);
+            foreach (var property in type.GetProperties(BindingFlags.Public | BindingFlags.Instance))
+            {
+                // p => p.XXX;
+                var getParameter = Expression.Parameter(typeof(T));
+                var getBody = Expression.Convert(Expression.Property(getParameter, property.Name), typeof(object));
+                var getDelegate = Expression.Lambda<Func<T, object>>(getBody, getParameter).Compile();
+                GetDelegateDict.Add(property.Name, getDelegate);
+
+                // (p,v) => p.XXX = v;
+                var setParameter1 = Expression.Parameter(typeof(T));
+                var setParameter2 = Expression.Parameter(typeof(object));
+                var setBody = Expression.Assign(Expression.Property(setParameter1, property.Name), Expression.Convert(setParameter2, property.PropertyType));
+                var setDelegate = Expression.Lambda<Action<T, object>>(setBody, setParameter1, setParameter2).Compile();
+                SetDelegateDict.Add(property.Name, setDelegate);
+                
+                var columnAttribute = property.GetCustomAttribute<Column>();
+                if (columnAttribute != null && columnAttribute.Name != property.Name)
+                {
+                    GetDelegateDict.Add(columnAttribute.Name, getDelegate);
+                    SetDelegateDict.Add(columnAttribute.Name, setDelegate);
+                }
+            }
+        }
+
+        public static void SetProperty(T obj, string propertyName, object value)
+        {
+            if (!SetDelegateDict.ContainsKey(propertyName))
+            {
+                return;
+            }
+
+            SetDelegateDict[propertyName].Invoke(obj, value);
+        }
+
+        public static object GetProperty(T obj, string propertyName)
+        {
+            return GetDelegateDict[propertyName].Invoke(obj);
+        }
+
+        public static T FillEntity(DataRow row)
+        {
+            if (row == null)
+            {
+                return null;
+            }
+
+            T entity = new T();
+            foreach (var column in row.Table.Columns)
+            {
+                SetProperty(entity, column.ToString(), row[column.ToString()]);
+            }
+            return entity;
+        }
+
+        public static void FillDataRow(T entity, ref DataRow dataRow)
+        {
+            foreach (var column in dataRow.Table.Columns)
+            {
+                var columnName = column.ToString();
+                if (GetDelegateDict.ContainsKey(columnName))
+                {
+                    dataRow[columnName] = GetProperty(entity, columnName);
+                }
+            }
+        }
+    }
+}

f530d2df3c75ca029690ae481b4d62cd5b10296f
yangzw <yangzw@Ctrip.com>
2015-07-08 13:31:07 +0800

    Add nothing.

---
+++ Infrastructure/Attribute/MethodInvokeAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Runtime.Remoting.Messaging;
+using System.Runtime.Remoting.Proxies;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Attribute
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class MethodInvokeAttribute : ProxyAttribute
+    {
+        public void AAA()
+        {
+            Thread.CurrentContext.co
+        }
+    }
+}
